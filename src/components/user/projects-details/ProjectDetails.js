85e00b44e5693b0951a8b36264f9d9f9e6464a48
Cendel Altunisik <cendelaltinisik@gmail.com>
2023-02-26 20:02:00 +0100

    v5

--- src/components/user/projects-details/ProjectDetails.js
+++ src/components/user/projects-details/ProjectDetails.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./projectDetails.scss";
 import image from "../../../assets/img/breads-1867459_1920.jpg";
 import imageRounded from "../../../assets/img/rounded-bottom.svg";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import ProgressBar from "react-bootstrap/ProgressBar";
 
 const ProjectDetails = () => {
   useEffect(() => {
@@ -10,15 +11,16 @@ const ProjectDetails = () => {
     const handleScroll = () => {
       const scrollPos = window.scrollY;
       const detailsEl = document.querySelector(".project-details");
-      console.log("scroll: " + scrollPos);
-      console.log(window.innerHeight);
-      console.log(window.innerHeight - scrollPos);
+      const detailsContainerEl = document.querySelector(
+        ".project-screen-image"
+      );
 
       if (window.innerHeight >= scrollPos) {
         detailsEl.style.marginTop = `${window.innerHeight - scrollPos}px`;
       } else {
         detailsEl.style.marginTop = "0px";
       }
+      detailsContainerEl.style.opacity = `0.${window.innerHeight - scrollPos}`;
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -33,17 +35,39 @@ const ProjectDetails = () => {
       </div>
 
       <div>
-        <img src={image} alt="My Image" className="project-screen-image" />
+        <img src={image} alt="bakery" className="project-screen-image" />
       </div>
       <Container fluid="lg" className="project-details">
-        <div className="first-part">
-          <div className="title">
-            <h5>sdfsadf</h5>
-            <h5></h5>
-          </div>
-          <div className="info"></div>
+        <div className="title">
+          <h5>Bäkerei</h5>
         </div>
-        <div className="second-part"></div>
+        <div className="details-box">
+          <div className="first-part">
+            <div className="info-left">
+              Wir machen Süßigkeiten - anders. no sweets steht für vegane,
+              glutenfreie Snacks ohne Industriezucker. Keine Kompromisse - Das
+              geht. Und schmeckt! Unsere Snack Bites werden mit viel Liebe in
+              Deutschland aus hochwertigen Zutaten hergestellt und ermöglichen
+              Dir ein unbeschwertes Naschen ohne Heißhungerattacken!
+            </div>
+            <div className="info-right">Creavision GmBH</div>
+          </div>
+          <div className="second-part">
+            <div className="left">
+              <ProgressBar
+                animated={false}
+                now={4}
+                max={7}
+                label={"Teilnehmer/-innen"} variant={"success"}
+              />
+              <div></div>
+            </div>
+            <div className="right">
+              <Button>Unterstützen</Button>
+              <Button>Abonieren</Button>
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
