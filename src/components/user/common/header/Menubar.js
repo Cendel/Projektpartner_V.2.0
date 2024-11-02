a92fd51d14a691177ee22b60a9818139616439bf
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-11-03 00:50:59 +0100

    deletes some redundant comments

--- src/components/user/common/header/Menubar.js
+++ src/components/user/common/header/Menubar.js
@@ -5,8 +5,7 @@ import "./header.scss";
 import MenuDropdown from "./MenuDropdown";
 
 const Menubar = () => {
-  const { pathname } = useLocation(); //problem: hangi sayfada bulunuyorsak, o sayfanin buton aktif görünsün: Bunun icin önce react-router-dom dan gelen useLocation hook unu kullanarak useLocation dan bulundugumuzu path i aliyoruz, daha sonra ilgili nav.link lere kodumuzu yaziyoruz.
-
+  const { pathname } = useLocation();
   return (
     <div className="menubar">
       <Navbar>
@@ -19,11 +18,6 @@ const Menubar = () => {
             >
               Projekte
             </Nav.Link>
-            {/*
-            <Nav.Link as={Link} to="/about" active={pathname === "/about"}>
-              Über uns
-            </Nav.Link>
-  */}
             <Nav.Link as={Link} to="/contact" active={pathname === "/contact"}>
               Kontakt
             </Nav.Link>
